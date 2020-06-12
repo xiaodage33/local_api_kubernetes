3dac817c162bd200f1d826b8f3f8ad1dd50387ac
xiaodage33 <542035984@qq.com>
2020-06-12 18:46:51 +0800

    抛出pod错误名字和数量 时间太长需要优化。。

--- k8s/views.py
+++ k8s/views.py
@@ -23,7 +23,7 @@ def getLog(request):
             dic2['status'] =get_data[i]
             x_data = dict(list(dic1.items()) + list(dic2.items()))
             data_list.append(x_data)
-    print("niu",data_list)
+    # print("niu",data_list)
     return JsonResponse({'data':data_list})
 #跟进pod名字进行查询日志
 # kubectl logs  -n `kubectl  get pods --all-namespaces | grep etcd-master| awk '{print $1}'`  etcd-master
@@ -34,7 +34,7 @@ def getLogInfo(request):
     info = info.decode()
     cmd_1 = "{print $1}"  #不然命令不识别print $1
     cmd = "kubectl logs  -n `kubectl  get pods --all-namespaces | grep {0} | awk '{1}'`  {0}  --tail 2000 ".format(info,cmd_1)
-    print("命令是",cmd)
+    # print("命令是",cmd)
 
 
 
@@ -43,19 +43,24 @@ def getLogInfo(request):
     return JsonResponse({'data':data})
 
 def getErrorLog(request):
-    cmd = "for i in `kubectl get pods -n scf |sed -e 1d | awk '{print $1}'`;do kubectl logs $i -n scf  | grep Exception:  >> /dev/null 2>&1 ; if [ `echo $?` -eq 0 ];then echo '有问题'$i ;fi  done | wc -l"
+    #抛出有问题数量
+    cmd = "for i in `kubectl get pods -n scf |sed -e 1d | awk '{print $1}'`;do kubectl logs $i -n scf  | grep Exception:  >> /dev/null 2>&1 ; if [ `echo $?` -eq 0 ];then echo $i ;fi  done | wc -l"
     data_num = ssh_link.ssh_linking(cmd)
     #抛出多有错误的pod名字
     cmd1 = "for i in `kubectl get pods -n scf |sed -e 1d | awk '{print $1}'`;do kubectl logs $i -n scf  | grep Exception:  >> /dev/null 2>&1 ; if [ `echo $?` -eq 0 ];then echo $i ;fi  done"
 
     # cmd1 = "for i in `kubectl get pods -n scf |sed -e 1d | awk '{print $1}'`;do kubectl logs $i -n scf  | grep Exception:  >> /dev/null 2>&1 ; if [ `echo $?` -eq 0 ];then echo $i ;fi  done"
+    # data_podname = ssh_link.ssh_linking("kubectl get pods -n scf |sed -e 1d | awk '{print $1}'") #暂时测试所有pod名字抛出来
 
-    data_podname = ssh_link.ssh_linking("kubectl get pods -n scf |sed -e 1d | awk '{print $1}'") #暂时测试所有pod名字抛出来
+    data_podname = ssh_link.ssh_linking(cmd1) #暂时测试所有pod名字抛出来
+    print("====",data_podname)
+
     get_data = data_podname.split()
-    print("哈哈",data_podname)
+    # print("哈哈",get_data)
     data_podname_list = []
+
     for key in get_data:
-        print("乐乐",key)
+        # print("乐乐",key)
         dic = {}
         dic['data_podname'] = key
         data_podname_list.append((dic))
